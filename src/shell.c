00d70946a6092da99c9747b6da56e39980c8319a
awei123 <awei.hsu@seed.net.tw>
2014-10-23 15:26:32 +0800

    Update shell.c

--- src/shell.c
+++ src/shell.c
@@ -1,22 +1,28 @@
+
+
 #include "shell.h"
-#include <stddef.h>
+#include
 #include "clib.h"
-#include <string.h>
+#include
 #include "fio.h"
 #include "filesystem.h"
 
+#include
 #include "FreeRTOS.h"
 #include "task.h"
 #include "host.h"
-
-#include "bufbomb.h"
+//#include
+//#include
+include "stdarg.h"
 
 typedef struct {
-	const char *name;
-	cmdfunc *fptr;
-	const char *desc;
+const char *name;
+cmdfunc *fptr;
+const char *desc;
+
 } cmdlist;
 
+extern int romfs_r[256], fs_opening;
 void ls_command(int, char **);
 void man_command(int, char **);
 void cat_command(int, char **);
@@ -26,158 +32,204 @@ void help_command(int, char **);
 void host_command(int, char **);
 void mmtest_command(int, char **);
 void test_command(int, char **);
-void bufbomb_command(int, char **);
+void awei_command(int, char **);
 
 #define MKCL(n, d) {.name=#n, .fptr=n ## _command, .desc=d}
 
 cmdlist cl[]={
-	MKCL(ls, "List directory"),
-	MKCL(man, "Show the manual of the command"),
-	MKCL(cat, "Concatenate files and print on the stdout"),
-	MKCL(ps, "Report a snapshot of the current processes"),
-	MKCL(host, "Run command on host"),
-	MKCL(mmtest, "heap memory allocation test"),
-	MKCL(help, "help"),
-	MKCL(test, "test new function"),
-	MKCL(bufbomb, "buffer overflow attack bomb")
+MKCL(ls, "List directory and file name"),
+MKCL(man, "Show the manual of the command"),
+MKCL(cat, "Concatenate files and print on the stdout"),
+MKCL(ps, "Report a snapshot of the current processes"),
+MKCL(host, "Run command on host"),
+MKCL(mmtest, "heap memory allocation test"),
+MKCL(help, "help"),
+MKCL(test, "test new function"),
+MKCL(awei, "new command of tested")
 };
-
-int parse_command(char *str, char *argv[]){
-	int b_quote=0, b_dbquote=0;
-	int i;
-	int count=0, p=0;
-	for(i=0; str[i]; ++i){
-		if(str[i]=='\'')
-			++b_quote;
-		if(str[i]=='"')
-			++b_dbquote;
-		if(str[i]==' '&&b_quote%2==0&&b_dbquote%2==0){
-			str[i]='\0';
-			argv[count++]=&str[p];
-			p=i+1;
-		}
-	}
-	/* last one */
-	argv[count++]=&str[p];
-
-	return count;
+int parse_command(char str, char *argv[]){
+int b_quote=0, b_dbquote=0;
+int i;
+int count=0, p=0;
+for(i=0; str[i]; ++i){
+if(str[i]=='\'')
+++b_quote;
+if(str[i]=='"')
+++b_dbquote;
+if(str[i]==' '&&b_quote%2==0&&b_dbquote%2==0){
+str[i]='\0';
+argv[count++]=&str[p];
+p=i+1;
 }
+}
+/ last one */
+argv[count++]=&str[p];
 
-void ls_command(int n, char *argv[]){
+    return count;
 
 }
+void ls_command(int n, char argv[])
+{
+fio_printf(1, "** Testing 01 ls***\n\r");
 
+  if (n==1)
+  {
+     fio_printf(2, "\r\n Usage : ls (directory/) and ls romfs/\r\n");
+
+  return ;
+  }
+  char buf[1024*16];
+ fio_printf(1, " shell test 02 \r\n ");
+ if ( argv[1] )
+ {
+    int FileNumber = fs_open(argv[1], O_LS, O_RDONLY);
+    int count;
+    if (FileNumber == 0 )
+
+       fio_printf(1, " shell test 03 \r\n");
+
+    else
+
+       for (int i=0 ; i<FileNumber ; i++)
+
+           while ((count = fio_read(romfs_r[i], buf, sizeof(buf)))> 0)
+           {
+                 fio_write(1, (buf+count-15), 14);
+                 fio_printf(1, "shell test 05 \r\n");
+           }
+
+ }
+ else
+       return;
+
+}
 int filedump(const char *filename){
-	char buf[128];
+char buf[128];
 
-	int fd=fs_open(filename, 0, O_RDONLY);
+    int filedp = fs_open(filename, 0, O_RDONLY);
 
-	if(fd==OPENFAIL)
-		return 0;
-
-	fio_printf(1, "\r\n");
-
-	int count;
-	while((count=fio_read(fd, buf, sizeof(buf)))>0){
-		fio_write(1, buf, count);
-	}
-
-	fio_close(fd);
-	return 1;
-}
-
-void ps_command(int n, char *argv[]){
-	signed char buf[1024];
-	vTaskList(buf);
-        fio_printf(1, "\n\rName          State   Priority  Stack  Num\n\r");
-        fio_printf(1, "*******************************************\n\r");
-	fio_printf(1, "%s\r\n", buf + 2);	
-}
-
-void cat_command(int n, char *argv[]){
-	if(n==1){
-		fio_printf(2, "\r\nUsage: cat <filename>\r\n");
-		return;
-	}
-
-	if(!filedump(argv[1]))
-		fio_printf(2, "\r\n%s no such file or directory.\r\n", argv[1]);
-}
-
-void man_command(int n, char *argv[]){
-	if(n==1){
-		fio_printf(2, "\r\nUsage: man <command>\r\n");
-		return;
-	}
-
-	char buf[128]="/romfs/manual/";
-	strcat(buf, argv[1]);
-
-	if(!filedump(buf))
-		fio_printf(2, "\r\nManual not available.\r\n");
-}
-
-void host_command(int n, char *argv[]){
-    int i, len = 0, rnt;
-    char command[128] = {0};
-
-    if(n>1){
-        for(i = 1; i < n; i++) {
-            memcpy(&command[len], argv[i], strlen(argv[i]));
-            len += (strlen(argv[i]) + 1);
-            command[len - 1] = ' ';
-        }
-        command[len - 1] = '\0';
-        rnt=host_action(SYS_SYSTEM, command);
-        fio_printf(1, "\r\nfinish with exit code %d.\r\n", rnt);
-    } 
-    else {
-        fio_printf(2, "\r\nUsage: host 'command'\r\n");
-    }
-}
-
-void help_command(int n,char *argv[]){
-	int i;
-	fio_printf(1, "\r\n");
-	for(i=0;i<sizeof(cl)/sizeof(cl[0]); ++i){
-		fio_printf(1, "%s - %s\r\n", cl[i].name, cl[i].desc);
-	}
-}
-
-void test_command(int n, char *argv[]) {
-    int handle;
-    int error;
+    if(filedp == OPENFAIL)
+            return 0;
 
     fio_printf(1, "\r\n");
 
-    handle = host_action(SYS_OPEN, "output/syslog", 8);
-    if(handle == -1) {
-        fio_printf(1, "Open file error!\n\r");
-        return;
+    int count;
+    while((count = fio_read(filedp, buf, sizeof(buf)))>0){
+            fio_write(1, buf, count -12);
     }
 
-    char *buffer = "Test host_write function which can write data to output/syslog\n";
-    error = host_action(SYS_WRITE, handle, (void *)buffer, strlen(buffer));
-    if(error != 0) {
-        fio_printf(1, "Write file error! Remain %d bytes didn't write in the file.\n\r", error);
-        host_action(SYS_CLOSE, handle);
-        return;
+    fio_close(filedp);
+    return 1;
+
+}
+void ps_command(int n, char argv[]){
+signed char buf[1024];
+vTaskList(buf);
+fio_printf(1, "\n\rName State Priority Stack Num\n\r");
+fio_printf(1, "******************************************\n\r");
+fio_printf(1, "%s\r\n", buf + 2);
+}
+
+void cat_command(int n, char *argv[]){
+if(n==1){
+fio_printf(2, "\r\nUsage: cat \r\n");
+return;
+}
+
+    if(!filedump(argv[1]))
+            fio_printf(2, "\r\n%s no such file or directory.\r\n", argv[1]);
+
+}
+void man_command(int n, char *argv[]){
+if(n==1){
+fio_printf(2, "\r\nUsage: man \r\n");
+return;
+}
+
+    char buf[128]="/romfs/manual/";
+    strcat(buf, argv[1]);
+
+    if(!filedump(buf))
+            fio_printf(2, "\r\nManual not available.\r\n");
+
+}
+
+void host_command(int n, char *argv[]){
+int i, len = 0, rnt;
+char command[128] = {0};
+
+if(n>1){
+    for(i = 1; i < n; i++) {
+        memcpy(&command[len], argv[i], strlen(argv[i]));
+        len += (strlen(argv[i]) + 1);
+        command[len - 1] = ' ';
     }
-
-    host_action(SYS_CLOSE, handle);
+    command[len - 1] = '\0';
+    rnt=host_action(SYS_SYSTEM, command);
+    fio_printf(1, "\r\nfinish with exit code %d.\r\n", rnt);
+}
+else {
+    fio_printf(2, "\r\nUsage: host 'command'\r\n");
 }
 
-void bufbomb_command(int n, char *argv[]) {
-	bufbomb();
 }
+void test_command(int n, char *argv[])
+{
+// int handle,error;
+int handlepn = 15;
+int lc , paramterARG = 3;
+int count ;
 
-cmdfunc *do_command(const char *cmd){
+fio_printf(1, "test command 01\r\n");
+//handle = host_action(SYS_SYSTEM, "mkdir -p output");
+for (count = 2; count <=  handlepn;  )
+{
+  for (lc = 2; lc <= paramterARG -1 ; lc ++)
+  {
+     if ( paramterARG % lc == 0)
+        break ;
+  }
+  if ( lc == paramterARG)
+     {
+     fio_printf(1,"%d\r\n", paramterARG);
+     count++;
+     }
+   paramterARG++;
+}
+return;
 
-	int i;
+}
+void awei_command(int n, char argv[])
+{
+fio_printf(1, "** aweicommand testing of p78991082***\n\r");
 
-	for(i=0; i<sizeof(cl)/sizeof(cl[0]); ++i){
-		if(strcmp(cl[i].name, cmd)==0)
-			return cl[i].fptr;
-	}
-	return NULL;	
+  if (n==1)
+  {
+     fio_printf(2, "\r\n Usage : ls (directory/) and ls romfs/\r\n");
+
+  return ;
+  }
+  char buf[1024*16];
+ fio_printf(1, " shell test 022 \r\n ");
+ if ( argv[1] )
+ {
+    int FileNumber = fs_open(argv[1], O_LS, O_RDONLY);
+    int count;
+    if (FileNumber == 0 )
+
+       fio_printf(1, " awei shell test 0032 \r\n");
+
+    else
+       for (int i=0 ; i<FileNumber ; i++)
+
+           while ((count = fio_read(romfs_r[i], buf, sizeof(buf)))> 0)
+           {
+                 fio_write(1, (buf+count-15), 14);
+                 fio_printf(1, "shell test 05 \r\n");
+           }
+
+ }
+ else
+   return;
+
 }
